5ebf95e5d7858b5addd06766ea5a9e3b754d5693
the8thbit <the8thbit@gmail.com>
2021-11-11 21:04:34 -0600

    move utility functions and data into its own directory

---
+++ src/Utilities/utilities.js
@@ -0,0 +1,57 @@
+const MONTHS = {
+  JANUARY: 0,
+  FEBRUARY: 1,
+  MARCH: 2
+}
+
+const convertDollarsToPoints = (dollars) => (
+  Math.min(
+    50,
+    Math.max(
+      0,
+      Math.floor(dollars) - 50,
+    )
+  ) +
+  Math.max(
+    (Math.floor(dollars) - 100) * 2,
+    0
+  )
+);
+
+const calculatePointsFromPurchases = (purchases) => (
+  purchases.reduce((acc, purchase) => (
+    acc + convertDollarsToPoints(purchase.price)
+  ), 0)
+);
+
+const filterPurchasesByMonth = (purchases, monthIndex) => (
+  purchases.filter((purchase) => {
+    const purchaseDate = new Date(purchase.date);
+    const purchaseMonth = purchaseDate.getMonth();
+    return (purchaseMonth === monthIndex);
+  })
+);
+
+const calculatePointsFromPurchasesByMonth = (purchases, monthIndex) => (
+  calculatePointsFromPurchases(
+    filterPurchasesByMonth(purchases, monthIndex)
+  )
+);
+
+const processPointsData = (purchases) => {
+  const jan = calculatePointsFromPurchasesByMonth(purchases, MONTHS.JANUARY);
+  const feb = calculatePointsFromPurchasesByMonth(purchases, MONTHS.FEBRUARY);
+  const mar = calculatePointsFromPurchasesByMonth(purchases, MONTHS.MARCH);
+  const total = jan + feb + mar;
+
+  return { jan, feb, mar, total };
+}
+
+export {
+  MONTHS,
+  convertDollarsToPoints,
+  calculatePointsFromPurchases,
+  filterPurchasesByMonth,
+  calculatePointsFromPurchasesByMonth,
+  processPointsData
+}
